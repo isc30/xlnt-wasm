f5a2a831c0e56182aa02a3d1eff060da5c9b72c5
Ivan Sanz-Carasa <me@ivansanz.com>
2018-09-07 00:44:22 +0200

    first commit

---
+++ include/bindings/workbook.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <emscripten.h>
+#include <emscripten/bind.h>
+
+#include <xlnt/xlnt.hpp>
+
+void download(xlnt::workbook& workbook, const std::string& fileName)
+{
+    workbook.save(fileName);
+
+    EM_ASM_({
+        fileName = Pointer_stringify($0);
+        fileContent = [FS.readFile(fileName)];
+
+        mime = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+        file = new Blob(fileContent, { type: mime });
+        downloadUrl = URL.createObjectURL(file);
+
+        // Download the Blob
+        anchor = document.createElement('a');
+        anchor.download = fileName;
+        anchor.href = downloadUrl;
+        anchor.dataset.downloadurl = mime + ":" + fileName + ":" + downloadUrl;
+        document.body.appendChild(anchor);
+        anchor.click();
+        document.body.removeChild(anchor);
+    }, fileName.c_str());
+}
+
+EMSCRIPTEN_BINDINGS(workbook)
+{
+    emscripten::class_<xlnt::workbook>("workbook")
+
+        .constructor<>()
+
+        .function("active_sheet",
+            &xlnt::workbook::active_sheet)
+
+        .function("save",
+            static_cast<void(xlnt::workbook::*)(const std::string&) const>(
+                &xlnt::workbook::save))
+
+        .function("download",
+            &download)
+    ;
+}
---
+++ src/bindings.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+#include "bindings/workbook.hpp"
+//#include "bindings/worksheet.hpp"
+//#include "bindings/cell.hpp"
